48ed46b72fdd51f03b3a0c03a8f6fd1a841dc526
clay-carper <ccarper2@uwyo.edu>
2023-02-11 15:30:07 -0700

    Updated `cocoon` security

--- src/cocoon.rs
+++ src/cocoon.rs
@@ -2,6 +2,7 @@ use cocoon::{Cocoon, Error};
 use std::fs::{File, read, remove_file};
 use std::io::Write;
 
+
 // Usage: Revised to drop function inputs, we'll always operate on
 // the same file names and should consistently delete the temporary
 // unencrypted database after creating an updated, encrypted copy.
@@ -12,19 +13,21 @@ pub fn encrypt_data() {
     let data = read("dd.db").unwrap();
     let mut file = File::create("dd-enc.db").unwrap();
 
+    let secret = read("secret.txt").unwrap();
+    let temp_secret = &secret;
+    let secret_bytes: &[u8] = &temp_secret;
+
     // Create a cocoon, the container we will encrypt
     // For the initial run, a password is used.
     // This has obvious problems, but serves as a
     // proof of concept.
     // Future implementations should utilize a file
     // that users would need to provide.
-    let cocoon = Cocoon::new(b"password");
+    let cocoon = Cocoon::new(secret_bytes);
 
     cocoon.dump(data, &mut file).unwrap_or_else(|error:Error| println!("Error while encrypting file: {:?}", error));
 
     remove_file("dd.db").unwrap();
-
-
 }
 
 
@@ -35,7 +38,13 @@ pub fn encrypt_data() {
 // this potential data leakage.
 pub fn decrypt_data() {
     let mut file = File::open("dd-enc.db").unwrap();
-    let cocoon = Cocoon::new(b"password");
+    //let cocoon_secret = read("secret.txt").unwrap().parse().unwrap();
+
+    let secret = read("secret.txt").unwrap();
+    let temp_secret = &secret;
+    let secret_bytes: &[u8] = &temp_secret;
+
+    let cocoon = Cocoon::new(secret_bytes);
 
     let data = cocoon.parse(&mut file).unwrap();
     // Create a temp file that is named consistently with
@@ -47,3 +56,10 @@ pub fn decrypt_data() {
     temp_file.write(&data).unwrap();
 }
 
+// // A function that reads in a file and uses the contents to create a cocoon,
+// // the secure container used for file encryption/decryption.
+// fn make_cocoon() -> cocoon::Cocoon {
+//     let mut file = File::open("secret.txt").to;
+//     let cocoon
+
+// }
